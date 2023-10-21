a54e3cbd5406497d6a8eb657f2db5210e7c888bb
Richard1227 <richard1227123@gmail.com>
2023-10-21 17:52:47 -0300

    update phoenixa rev.6

--- js/script_menu.js
+++ js/script_menu.js
@@ -1,19 +1,21 @@
 // script responsavel pela automaçãõ do menu hamburger
 
 const btnMobile = document.getElementById('btn-mobile')
-// const >> declara nossa variavel no uso do decorre r do codigo
+// const >> declara nossa variavel no uso do decorrer do codigo
 
 function toggleMenu(event) {
-   if (event.type === 'toutchstart') event.preventDefalt();
-   const nav = document.getElementaryById('nav');
-event.currentTarget.setAtribute('aria-expanded', active);
+   if (event.type === 'toutchstart') event.preventDefault();
+   const nav = document.getElementById('nav');
+   nav.classList.toggle ('active');
+   const active = nav.classList.contains('active');
+event.currentTarget.setAttribute('aria-expanded', active);
  if (active ) {
-    event.currentTarget.setAtribute('aria-label', 'fechar menu');
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
  } else {
-    event.currentTarget.setAtribute( 'aria-label', 'abrir menu' );
+    event.currentTarget.setAttribute( 'aria-label', 'Abrir Menu' );
  }
  
 }
 
 btnMobile.addEventListener('click', toggleMenu)
-btnMobile;addEventListener('toutchstart', toggleMenu)
+btnMobile.addEventListener('toutchstart', toggleMenu)
